25f6fa3dea415e0a9f33d9b1c26a9f14f035470d
borapop <nikita.skryt@gmail.com>
2015-10-19 18:40:36 +0500

    Frequency number input, visualization with canvas.

--- generator.js
+++ generator.js
@@ -6,11 +6,54 @@ function generate(type, frequency) {
   
 
 
-  var audioContext = new window.AudioContext();
+  var audioContext = new window.AudioContext() || window.webkitAudioContext();
   oscillator = audioContext.createOscillator();
+
+
+  
+
+
+  var analyser = audioContext.createAnalyser();
+    analyser.fftSize = 256;
+    var bufferLength = analyser.frequencyBinCount;
+    console.log(bufferLength);
+    var dataArray = new Uint8Array(bufferLength);
+    WIDTH = 
+    HEIGHT = 500;
+    var canvasContext = document.getElementsByTagName('canvas')[0].getContext('2d');
+    canvasContext.clearRect(0, 0, WIDTH, HEIGHT);
+
+
   var gainNode = audioContext.createGain();
-  oscillator.connect(gainNode);
+  oscillator.connect(analyser);
+  analyser.connect(gainNode);
   gainNode.connect(audioContext.destination);
+  function draw() {
+    drawVisual = requestAnimationFrame(draw);
+
+    analyser.getByteFrequencyData(dataArray);
+
+    canvasContext.fillStyle = 'rgb(0, 0, 0)';
+    canvasContext.fillRect(0, 0, WIDTH, HEIGHT);
+    var barWidth = (WIDTH / bufferLength) * 2.5;
+      var barHeight;
+      var x = 0;
+
+      for(var i = 0; i < bufferLength; i++) {
+          barHeight = dataArray[i]/2;
+
+          canvasContext.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+          canvasContext.fillRect(x,HEIGHT-barHeight/2,barWidth,barHeight);
+
+          x += barWidth + 1;
+        }
+      };
+
+  draw();
+
+  
+
+
   oscillator.type = type; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
   oscillator.frequency.value = frequency;
   
@@ -18,7 +61,7 @@ function generate(type, frequency) {
     oscillator.start();
     };
 
-  this.stop = function(){
+  this.stop = function() {
     oscillator.on();
   };
 
@@ -44,28 +87,42 @@ function generate(type, frequency) {
 
 
   var typeRange = document.getElementById('typeRange');
+  var typeString = document.getElementById('typeString');
 
   typeRange.onchange = function() {
-
+    
     if (typeRange.value == 1) {
         generator.type('sine');
+        typeString.innerHTML = 'sine';
     } 
     else if (typeRange.value == 2) {
         generator.type('square');
+        typeString.innerHTML = 'square';
     } 
     else if (typeRange.value == 3) {
           generator.type('sawtooth');
+          typeString.innerHTML = 'sawtooth';
       } 
       else {
           generator.type('triangle');
+          typeString.innerHTML = 'triangle';
       }
-
   };
 
   var frequencyRange = document.getElementById('frequencyRange');
+  var frequencyNumber = document.getElementById('frequencyNumber');
 
   frequencyRange.onchange = function() {
+
     generator.frequency(frequencyRange.value);
+    frequencyNumber.value = frequencyRange.value;
+
+  };
+ 
+  frequencyNumber.onchange = function() {
+
+    generator.frequency(frequencyNumber.value);
+    frequencyRange.value = frequencyNumber.value;
 
   };
   
